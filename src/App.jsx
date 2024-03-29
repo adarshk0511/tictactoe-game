712b921bd6dd7c38dc2d8666192fa7de44b9d965
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2024-03-29 19:47:53 +0530

    introduced a player to the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './styles.scss'
 import Board from './components/Board';
 
 function App() {
 
+ const [counter, setCounter]=useState();
+
+  const onBtnClick = () =>{
+    setCounter(currentCounter =>{
+      return currentCounter + 1;
+    });
+  };
+
+
+
   return(
     <div className='app'> 
-      <Board/>
+      <button onClick={onBtnClick}>Click Me!!</button>
+      <div>{counter}</div>
     </div> 
     
   );
